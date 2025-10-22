5497092b58cfdf7c95cd6a796010efd47f038644
Dania Farooq <danielfaruk299@gmail.com>
2025-10-22 22:43:38 +0500

    Add explanation

--- 02_Variables_Data_types/01_Variables_in_Python.py
+++ 02_Variables_Data_types/01_Variables_in_Python.py
@@ -1,1 +1,17 @@
+"""
+VARIABLES IN PYTHON
+Variables are containers for storing data values.
+Think of them as labeled boxes that hold information.
+"""
+
+print("=== MASTERING VARIABLES IN PYTHON ===\n")
+
+# =============================================
+# 1. WHAT ARE VARIABLES?
+# =============================================
+print("1. WHAT ARE VARIABLES?")
+print("   - Named containers that store data")
+print("   - Like labeled boxes holding information")
+print("   - Can store different types of data")
+print("   - Values can be changed (they're variable!)\n")
 
