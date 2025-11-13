e674fc65df53a70387a576bbea6f1e676ccfe2d5
Dania Farooq <danielfaruk299@gmail.com>
2025-11-13 13:58:23 +0500

    Update Python_practice_zone.py

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -1,6 +1,9 @@
-# 
+# This file contains python practice questions from all the topics that we have learned in this repo.
+# For your convenience, Questions are organized seperately for each topic.
+# Solutions are the code lines without # symbol. 
+# =======================================
 # Escape characters and Print statement
-# 
+# =======================================
 
 # Q1: Print the following path using escape sequences:
 #     C:\Users\John\Desktop\File.txt
