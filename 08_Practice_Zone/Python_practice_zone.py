12d8b2c75e915aef4935c230549d8278108abaa6
Dania Farooq <danielfaruk299@gmail.com>
2025-11-13 13:54:42 +0500

    Add question 1

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -1,1 +1,7 @@
+# 
+# Escape characters and Print statement
+# 
 
+# Q1: Print the following path using escape sequences:
+#     C:\Users\John\Desktop\File.txt
+print("C:", "Users", "John", "Desktop", "File.txt", sep="\\")
