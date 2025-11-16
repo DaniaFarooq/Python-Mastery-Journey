5418027bef9b0017c840ed6cbca32242be8b65f8
Dania Farooq <danielfaruk299@gmail.com>
2025-11-16 18:01:36 +0500

    Question 5 added

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -32,3 +32,5 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 result = num1 + num2
 print(f"The sum of both numbers is: {result}")
+
+# Q5. Convert a string to an integer and vice versa.
