a8e18c6972a04454ff4b6c90bf5961e4b6ab41da
Dania Farooq <danielfaruk299@gmail.com>
2025-11-16 17:53:54 +0500

    Add question 3

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -8,14 +8,16 @@
 
 # Q1: Write a program that prints a path like this:
 # C:\Users\John\Desktop\File.txt using the appropriate escape sequences.
+
 print("C:\\Users\\John\\Desktop\\File.txt")
 
 # Q2: Write a Python program that prints a message with a double-quote character inside it.
 # For example: He said, "Hello!".
+
 msg = 'He said, "Hello!"'
 print(msg)
 
-# Q3: Print a message containing both single and double quotes:
-# She said, 'It's cold'.
+# Q3: Create a program that prints a message containing both single anddouble quotes, like this: She said, 'It's cold'.
+
 msg2 = "She said, 'It's cold'."
 print(msg2)
