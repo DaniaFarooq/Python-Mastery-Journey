95ec1db2d388a381ae391e5b29802688f96e96a7
Dania Farooq <danielfaruk299@gmail.com>
2025-11-19 13:10:14 +0500

    add question 12

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -90,3 +90,13 @@ print(f"Total points: {points}")
 # Arithematic and Assignment Operators
 # ============================================
 
+# Q12. Write a Python program that takes two numbers as input and performs the following operations: addition, subtraction, multiplication,
+# division, and modulus. Display the results.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
