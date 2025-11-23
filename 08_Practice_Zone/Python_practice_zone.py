1765f6cf72ba966c5cea07628cf91c2ecd6bdb17
Dania Farooq <danielfaruk299@gmail.com>
2025-11-23 14:32:13 +0500

    Add Q 25

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -190,3 +190,14 @@ if char in 'aeiou':
     print("The character is a vowel.")
 else:
     print("The character is a consonant.")
+
+# Q25. Write a program that takes two numbers as input and checks if the first number is divisible by the second.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if num2 == 0:
+    print("Error: Division by zero is not allowed.")
+elif num1 % num2 == 0:
+    print(f"{num1} is divisible by {num2}")
+else:
+    print(f"{num1} is not divisible by {num2}")
