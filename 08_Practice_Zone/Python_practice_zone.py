334e003b21649c97e4b76de9dad24dd16256fb72
Dania Farooq <danielfaruk299@gmail.com>
2025-11-26 18:05:16 +0500

    Add Q 31

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -254,3 +254,11 @@ if num1 % 5 == 0:
     print("number is divible by 5")
 else:
     print("number is not divible by 5")
+
+# Q31. Write a program to calculate bill. Ask the final amount from the user.You have to give discount and print the final bill after discount.
+# 50000 above - 30% discount
+# 40000 - 49999 - 25% discount
+# 30000 - 39999 - 20% discount
+# 10000 - 29999 - 10% discount
+# 1 - 9999 - No discount
+# Print the discount and the final amount to be paid.
