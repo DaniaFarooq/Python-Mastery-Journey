29ec3073f98cf374047433b00b01a921556ff8a3
Dania Farooq <danielfaruk299@gmail.com>
2025-12-02 17:10:04 +0500

    Q 41 added

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -432,3 +432,24 @@ else:
         print("Ticket price is 10")
     else:
         print("Ticket price is 7")
+
+# Q41. Write a program that calculates a person's BMI based on their height (in meters) and weight (in kilograms). Use the following formula: BMI = weight / (height^2). Then, classify the BMI according to the following ranges:
+# Underweight: BMI less than 18.5
+# Normal weight: BMI 18.5 - 24.9
+# Overweight: BMI 25 - 29.9
+# Obesity: BMI 30 or greater
+
+height = float(input("Enter height in meters: "))
+weight = float(input("Enter weight in kilograms: "))
+bmi = weight / (height ** 2)
+print("Your BMI is:", round(bmi, 2))
+if bmi < 18.5:
+    print("You are underweight.")
+else:
+    if bmi < 25:
+        print("You have a normal weight.")
+    else:
+        if bmi < 30:
+            print("You are overweight.")
+        else:
+            print("You are obese.")
