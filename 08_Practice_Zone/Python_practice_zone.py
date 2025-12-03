c1f55489834e2a8aa35b27e68b1831820bd7d394
Dania Farooq <danielfaruk299@gmail.com>
2025-12-03 16:04:42 +0500

    Added Q 50

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -529,3 +529,13 @@ while i <= 50:
         num = num + i
     i = i + 1
 print(f"the sum is {num}")
+
+# Q50. Calculate how many numbers are divisible by 6 and 7 between 1 to 200.
+
+i = 1
+count = 0
+while i <= 200:
+    if i % 6 == 0 and i % 7 == 0:
+        count = count + 1
+    i = i + 1
+print(f"the count is {count}")
