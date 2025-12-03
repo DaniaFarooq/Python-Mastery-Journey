4d6559ed2e943da180b9c860989c2b0871ba4cd8
Dania Farooq <danielfaruk299@gmail.com>
2025-12-03 16:00:15 +0500

    Q 47 added

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -499,3 +499,13 @@ while i <= 10:
     total = total * i
     i = i + 1
 print(f"the total product is {total}")
+
+# Q47. Calculate how many numbers are divisible by 7 from 1 to 100.
+
+i = 1
+count = 0
+while i <= 100:
+    if i % 7 == 0:
+       count = count + 1
+    i = i + 1
+print(f"the value is {count}")
