4dc30b2852e16d0091f307c3eda1e68a5be2b179
Dania Farooq <danielfaruk299@gmail.com>
2025-12-03 14:51:03 +0500

    Q 45 added

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -481,3 +481,12 @@ i = start
 while i <= end:
     print(i)
     i = i + 1
+
+# Q45. Calculate the sum of all the numbers from 1 to 10.
+
+i = 1
+total = 0
+while i <= 10:
+    total = total + i
+    i = i + 1
+print(f"sum is {total}")
