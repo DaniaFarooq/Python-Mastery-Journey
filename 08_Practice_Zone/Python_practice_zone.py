988450acf9740f88d4a426449dab3a6435bfe6e2
Dania Farooq <danielfaruk299@gmail.com>
2025-12-06 21:58:29 +0500

    Af=dded Q 54

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -585,3 +585,9 @@ else:
 # =============================
 # Basic For Loop 
 # =============================
+
+# Q54. Ask a number from user. Print all the numbers from 1 to that number.
+
+num = int(input("Enter the number = "))
+for i in range(1, num + 1):
+    print(i)
