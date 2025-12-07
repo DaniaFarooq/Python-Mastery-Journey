081aedd589c64390ea004c1f2ede50a4332bbb42
Dania Farooq <danielfaruk299@gmail.com>
2025-12-07 12:07:21 +0500

    Added Q 61

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -625,7 +625,7 @@ count = 0
 for i in range(1, 101):
     if i % 7 == 0:
         count = count + 1
-print(f"count is {count}")
+print(f"Count is {count}")
 
 # Q60. Calculate how many numbers are divisible by both 6 and 7 between 1 to 200
 
@@ -633,4 +633,12 @@ count = 0
 for i in range(1, 201):
     if i % 6 == 0 and i % 7 == 0:
         count = count + 1
-    print(f"count is {count}")
+    print(f"Numbers divisible by 6 and 7 from 1 to 200 are {count} in total.")
+
+#Q61. Write a program to calculate the sum of all the numbers divisible by 4 from 20 to 50.
+
+total = 0
+for num in range(20, 51):  # 51 because range end is exclusive
+    if num % 4 == 0:
+        total += num
+print(f"Sum of numbers divisible by 4 from 20 to 50: {total}")
