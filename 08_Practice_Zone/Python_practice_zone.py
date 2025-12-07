8037661fe3729f1dab77a1d703f14e482723f954
Dania Farooq <danielfaruk299@gmail.com>
2025-12-07 12:10:58 +0500

    Added Q 64

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -662,3 +662,17 @@ table_of = int(input("Enter the number = "))
 for i in range(1, 11):
     table = table_of * i
     print(f"{table_of} * {i} = {table}")
+
+# Q64. Calculate factorial of a number entered by user.
+# Example:
+# Enter a number = 5
+# Factorial of a number means product of all the numbers from 1 to that
+# number.
+# 5 factorial = 5 x 4 x 3 x 2 x 1
+# Output = 120
+
+num = int(input("Enter the number = "))
+factorial = 1
+for i in range(1, num + 1):
+    factorial = factorial * i
+print(f"the value of the factorial is = {factorial}")
