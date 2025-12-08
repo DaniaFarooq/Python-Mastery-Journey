5db3862c417648df20209d626c7e0a7e021f4cee
Dania Farooq <danielfaruk299@gmail.com>
2025-12-08 21:20:46 +0500

    Q 69 added

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -730,3 +730,16 @@ for i in range(5):
         print(j, end="")
     print()
 print("\n" + "-"*20)
+
+# Q69: Create a pattern using nested loop
+# 1 1 1 1 1
+# 2 2 2 2 2 
+# 3 3 3 3 3
+# 4 4 4 4 4
+# 5 5 5 5 5
+
+for i in range(1, 6):                      
+    for j in range(5):                    
+        print(i, end="")                  
+    print()
+print("\n" + "-"*20)
