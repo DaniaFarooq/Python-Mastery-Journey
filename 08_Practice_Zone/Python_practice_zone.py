b41b801699540f4ea0504b9cf3f094597b1000e6
Dania Farooq <danielfaruk299@gmail.com>
2025-12-10 06:19:29 +0500

    Added Q 76

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -828,3 +828,15 @@ for i in range(1, 6):
     for j in range(i, 0, -1):             
         print(j, end="")
     print()
+
+# Q76: Print the following pattern.
+# 5
+# 5 4
+# 5 4 3
+# 5 4 3 2
+# 5 4 3 2 1
+
+for i in range(1, 6):
+    for j in range(5, 5 - i, -1):          
+        print(j, end="")
+    print()
