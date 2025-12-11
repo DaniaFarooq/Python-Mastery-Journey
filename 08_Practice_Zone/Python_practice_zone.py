d31bd5cad33476ca5df3c398531545e079a4caba
Dania Farooq <danielfaruk299@gmail.com>
2025-12-11 09:24:35 +0500

    Added Q 90

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -1026,3 +1026,17 @@ for i in range(4, 0, -1):
     for k in range(2 * i - 1):
         print("*", end="")
     print()
+
+# Q90: Print the following inverted pyramid.
+# * * * * * * * * *
+#  * * * * * * *
+#   * * * * *
+#    * * *
+#      *
+
+for i in range(5, 0, -1):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
