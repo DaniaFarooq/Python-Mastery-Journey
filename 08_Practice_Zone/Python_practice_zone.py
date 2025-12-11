1532f0cf8f62abd8994f802b4a66c66ec7f03035
Dania Farooq <danielfaruk299@gmail.com>
2025-12-11 09:19:59 +0500

    Added Q 89

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -994,3 +994,27 @@ for i in range(1, 6):
     for k in range(2 * i - 1):
         print("*", end="")
     print()
+
+# Q89: Print the following diamond pattern.
+#      *
+#    * * *
+#   * * * * *
+# * * * * * * *
+#* * * * * * * * * 
+#  * * * * * * *
+#   * * * * *
+#     * * *
+#       *
+
+for i in range(1, 6):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
+for i in range(4, 0, -1):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
