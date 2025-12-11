cb32ea94d6648a14cced9ff99b66968853615e5a
Dania Farooq <danielfaruk299@gmail.com>
2025-12-11 09:09:45 +0500

    Added Q 84

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -926,3 +926,15 @@ for i in range(1, 6):
         print(count, end="")
         count += 1
     print()
+
+# Q84: Print the following pattern.
+# 1
+# 2 1
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1
+
+for i in range(1, 6):
+    for j in range(i, 0, -1):             
+        print(j, end="")
+    print()
