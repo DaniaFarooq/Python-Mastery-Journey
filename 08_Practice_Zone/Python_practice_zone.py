b1f4292c33a29f7ced3bb58ec1668339a46abcb6
Dania Farooq <danielfaruk299@gmail.com>
2025-12-14 14:18:05 +0500

    Added Q 116

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -1292,3 +1292,19 @@ for i in a:
     if i in b and i not in c:
         c.append(i)
 print(c)
+
+# Q116. Write a Python code to find the second largest element in a list without sorting.
+
+def second_largest(lst):
+    first = second = float('-inf')
+    
+    for num in lst:
+        if num > first:
+            second = first
+            first = num
+        elif num > second and num != first:
+            second = num
+    
+    return second if second != float('-inf') else None
+
+
