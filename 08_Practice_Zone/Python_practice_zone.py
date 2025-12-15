3b00842291968a15de24a6c1b013583229cff5fc
Dania Farooq <danielfaruk299@gmail.com>
2025-12-15 08:36:59 +0500

    Added Q 126

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -1380,3 +1380,8 @@ print(new_list)
 
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 21]
 print(lst[::-1])
+
+# Q126. Write a python program to get every third element from a list using slicing.
+
+list1 = ["learning", "python", "with", "Dania", 2, 3, 4, 5,]
+print(list1[::3])
