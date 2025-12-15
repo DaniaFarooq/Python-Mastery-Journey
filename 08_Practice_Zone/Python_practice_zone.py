a8e858a182fa8ca0183e437015746d1be3ca1fc4
Dania Farooq <danielfaruk299@gmail.com>
2025-12-15 08:34:57 +0500

    Added Q 125

--- 08_Practice_Zone/Python_practice_zone.py
+++ 08_Practice_Zone/Python_practice_zone.py
@@ -1375,3 +1375,8 @@ print(new_list)
 # ================================
 # List Slicing Questions
 # ================================
+
+# Q125. Write a python program to reverse a list using slicing.
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 21]
+print(lst[::-1])
